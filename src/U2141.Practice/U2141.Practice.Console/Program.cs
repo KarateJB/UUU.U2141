8294f2ddcb563a555c0e41088549f3094b879c50
KarateJB <ppippenkimo@gmail.con>
2017-09-04 14:03:02 +0800

    Complete hashTable sample.

--- src/U2141.Practice/U2141.Practice.Console/Program.cs
+++ src/U2141.Practice/U2141.Practice.Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,17 +12,48 @@ namespace U2141.Practice
         static void Main(string[] args)
         {
             //Array copy
-            copyArray();
-
+            //copyArraySample();
 
             //Jagged array
-            jaggedArray();
+            //jaggedArraySample();
+
+            //ArrayList
+            //arrayListSample();
+
+            //HashTable
+            hashTableSample();
+
+
 
             Console.ReadKey();
         }
 
-        
-        private static void copyArray()
+        private static void hashTableSample()
+        {
+            System.Collections.Hashtable movies = new System.Collections.Hashtable();
+            movies.Add("10:00", "神力男超人");
+            movies.Add("13:00", "薑餅人：回家日");
+            movies.Add("16:00", "名偵探阿北");
+            Console.WriteLine(movies["13:00"]);
+        }
+
+        private static void arrayListSample()
+        {
+            ArrayList list= new ArrayList();
+            list.Add(100);
+            list.Add("demo");
+            list.Add(DateTime.Now);
+            Console.WriteLine((int)list[0] + 200);
+            Console.WriteLine(list[1]);
+            Console.WriteLine(((DateTime)list[2]).ToShortDateString());
+            //================================
+            int i = 100;
+            object obj = i;//int->object, boxing
+            int j = (int)obj; //object->int, unboxing
+            Console.WriteLine(j + 200);
+        }
+
+        private static void copyArraySample()
         {
             int[] list = new int[5] { 1, 2, 3, 4, 5 };
             int[] newList = new int[10];
@@ -34,7 +66,7 @@ namespace U2141.Practice
             }
         }
 
-        private static void jaggedArray()
+        private static void jaggedArraySample()
         {
             string[][] year = new string[12][];
             year[0] = new string[31];
