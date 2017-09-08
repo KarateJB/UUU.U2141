f4a08cc0a220fa8406ae40dd541ee0c8900211d9
KarateJB <ppippenkimo@gmail.con>
2017-09-08 10:12:42 +0800

    Abstract sample.

---
+++ src/U2141.Practice/U2141.Practice.Domain/AbsBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U2141.Practice.Domain
+{
+    public abstract class AbsBook
+    {
+        public abstract string Name { get; }
+        public abstract int Price { get; }
+        public abstract int Take(string name);
+    }
+}
---
+++ src/U2141.Practice/U2141.Practice.Domain/LordOfRings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U2141.Practice.Domain
+{
+    public class LordOfRings : AbsBook
+    {
+        public override string Name {
+            get => "Lord of rings";
+        }
+        public override int Price { get =>1000;  }
+
+        public override int Take(string name)
+        {
+            return this.Price;
+        }
+    }
+}
