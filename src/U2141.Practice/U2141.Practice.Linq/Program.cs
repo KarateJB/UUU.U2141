625547399d2cc6a2f4c46437d2c63fb79ef1b402
KarateJB <ppippenkimo@gmail.con>
2017-09-09 04:35:50 +0800

    Refactoring.

--- src/U2141.Practice/U2141.Practice.Linq/Program.cs
+++ src/U2141.Practice/U2141.Practice.Linq/Program.cs
@@ -10,14 +10,69 @@ namespace U2141.Practice.Linq
     {
         static void Main(string[] args)
         {
+            #region Sample #1
             var data = new List<string> { "JB", "Lily", "Leia" };
 
             var target = from x in data
-                     where x.Equals("JB")
-                     select x;
+                         where x.Equals("JB")
+                         select x;
 
             Console.WriteLine($"My name is {target.FirstOrDefault()}");
+            Console.WriteLine("=========================");
+            #endregion
 
+            #region Sample #2
+            var numData = new List<int>();
+            for (var i = 1; i <= 100; i++)
+            {
+                numData.Add(i);
+            }
+
+            var evens = from x in numData
+                        where x % 2 == 0
+                        select x;
+
+            Console.WriteLine($"Sum = {evens.Sum()}");
+            Console.WriteLine($"Average = {evens.Average()}");
+            Console.WriteLine("=========================");
+            #endregion
+
+
+            #region Sample #3 - Group
+            var dudes = new List<Dude> {
+
+                new Dude{ Name="Luke skywalker", City="Taipei", Age= 25},
+                new Dude{ Name="Leia skywalker", City="Tainan", Age= 25},
+                new Dude{ Name="Yoda", City="Taipei", Age= 100},
+                new Dude{ Name="Motha fuxxer", City="Tainan", Age= 30},
+                new Dude{ Name="Stan", City="Taipei", Age= 17}
+            };
+
+            var gp = from x in dudes
+                     group x by x.City;
+
+            gp.ToList().ForEach(x =>
+            {
+                Console.WriteLine($"{x.Key}");
+                x.ToList().ForEach(y => { Console.WriteLine($"{y.Name}({y.Age})"); });
+            });
+            Console.WriteLine("=========================");
+            #endregion
+
+            #region Lamda expression
+
+            var sortDudes = dudes.OrderByDescending(x => x.Age);
+            sortDudes.ToList().ForEach(x => Console.WriteLine($"{x.Name}({x.Age})"));
+            Console.WriteLine("=========================");
+            #endregion
         }
+
+        internal class Dude
+        {
+            public string Name { get; set; }
+            public string City { get; set; }
+            public int Age { get; set; }
+        }
+        
     }
 }
